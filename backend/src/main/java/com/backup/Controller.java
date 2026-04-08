949ca8dd349db80bebc3c369431f25be43b29e2c
yadakrishna245 <yadakrishna245@gmail.com>
2026-04-08 14:33:55 +0000

    Removed AWS secrets and secured credentials

--- backend/src/main/java/com/backup/Controller.java
+++ backend/src/main/java/com/backup/Controller.java
@@ -2,21 +2,46 @@ package com.backup;
 
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import java.io.*;
+import software.amazon.awssdk.auth.credentials.*;
+import software.amazon.awssdk.services.s3.*;
+import software.amazon.awssdk.services.s3.model.*;
 
 @RestController
 public class Controller {
 
+    private final String bucketName = "backup-app-storage";
+
+    private S3Client s3 = S3Client.builder()
+            .region(software.amazon.awssdk.regions.Region.AP_SOUTH_1)
+            .credentialsProvider(StaticCredentialsProvider.create(
+		AwsBasicCredentials.create(
+    System.getenv("AWS_ACCESS_KEY"),
+    System.getenv("AWS_SECRET_KEY")
+)
+            .build();
+
     @PostMapping("/upload")
     public String upload(@RequestParam("file") MultipartFile file) throws Exception {
-        File f = new File("/tmp/" + file.getOriginalFilename());
-        file.transferTo(f);
-        return "Uploaded";
+
+        s3.putObject(
+                PutObjectRequest.builder()
+                        .bucket(bucketName)
+                        .key(file.getOriginalFilename())
+                        .build(),
+                software.amazon.awssdk.core.sync.RequestBody.fromBytes(file.getBytes())
+        );
+
+        return "Uploaded to S3";
     }
 
     @GetMapping("/restore/{name}")
-    public byte[] restore(@PathVariable String name) throws Exception {
-        File f = new File("/tmp/" + name);
-        return java.nio.file.Files.readAllBytes(f.toPath());
+    public byte[] download(@PathVariable String name) throws Exception {
+
+        return s3.getObjectAsBytes(
+                GetObjectRequest.builder()
+                        .bucket(bucketName)
+                        .key(name)
+                        .build()
+        ).asByteArray();
     }
 }
